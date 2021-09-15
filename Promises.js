5de2c92966b1bd76d090479c2b1813ecb15490b4
himani17014 <36234455+himani17014@users.noreply.github.com>
2021-09-15 16:15:53 +0530

    added more scenarios with async await

--- Promises.js
+++ Promises.js
@@ -39,3 +39,73 @@ f();
  * 3. 1
  * try catch  Rejected
  */
+
+// if no try catch, we'll simply call the arrow fn inside .catch() handler 
+const f2 = async () => {
+
+    let p = new Promise(async (resolve, reject)=> {
+        a = 2
+        console.log("1.",a);
+        a = _wait(2000);
+
+        console.log("2.",a);
+        console.log("3.",await a);
+        a = await a
+        if(a==2){
+            resolve('Resolved');
+        } else reject('Rejected')
+    })
+    
+    val = await p
+        // .then(val => console.log("inside then ",val))
+        .catch(err => console.log(err));
+    // val =  await val
+    console.log("val", val);
+    
+}
+
+f2();
+
+
+/**
+ * 1. 2
+ * 2. Promise { <pending> }
+ * 3. 1
+ * Rejected
+ * val undefined
+ */
+
+
+//  no await before promise p
+const f3 = async () => {
+
+    let p = new Promise(async (resolve, reject)=> {
+        a = 2
+        console.log("1.",a);
+        a = _wait(2000);
+
+        console.log("2.",a);
+        console.log("3.",await a);
+        a = await a
+        if(a==2){
+            resolve('Resolved');
+        } else reject('Rejected')
+    })
+    
+    val = p
+        // .then(val => console.log("inside then ",val))
+        .catch(err => console.log(err));
+    // val =  await val
+    console.log("val", val);
+    
+}
+
+f3();
+
+/**
+ * 1. 2
+ * 2. Promise { <pending> }
+ * val Promise { <pending> }
+ * 3. 1
+ * Rejected
+ */
